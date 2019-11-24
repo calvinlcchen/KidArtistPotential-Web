415542f74015f96125324de38562c884e65e813c
calvinlcchen <42631868+calvinlcchen@users.noreply.github.com>
2019-11-24 12:50:04 +0800

    update for fb test

--- js/sketch.js
+++ js/sketch.js
@@ -211,3 +211,42 @@ function saveimgas() {
   download_link.download='download.png';  //set the download image filename as download.png
   download_link.click();
 }
+
+// Post a BASE64 Encoded PNG Image to facebook
+function PostImageToFacebook(authToken) {
+  //var canvas = document.getElementById("c");
+  //var imageData = outputImgContainer.elt.src.toDataURL("image/png");
+  try {
+      blob = outputImgContainer.elt.src; //dataURItoBlob(imageData);
+      console.log("blob:" + blob);
+  } catch (e) {
+      console.log(e);
+  }
+  var fd = new FormData();
+  fd.append("access_token", authToken);
+  fd.append("source", blob);
+  fd.append("message", "Photo Text");
+  try {
+      $.ajax({
+          url: "https://graph.facebook.com/me/photos?access_token=" + authToken,
+          type: "POST",
+          data: fd,
+          processData: false,
+          contentType: false,
+          cache: false,
+          success: function (data) {
+              console.log("success " + data);
+              $("#poster").html("Posted Canvas Successfully");
+          },
+          error: function (shr, status, data) {
+              console.log("error " + data + " Status " + shr.status);
+          },
+          complete: function () {
+              console.log("Posted to facebook");
+          }
+      });
+
+  } catch (e) {
+      console.log(e);
+  }
+}
